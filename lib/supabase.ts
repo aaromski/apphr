d0b508bd2752f39cdeb1535b38373106253fab47
aaromski <73103490+aaromski@users.noreply.github.com>
2026-09-09 20:24:14 -0600

    Correcion supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,9 +4,3 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-user
-
-HOTEL-2024
-admin@grandhotel.com
-12345
